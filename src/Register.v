f4f8a25bad201f9908a1deeed74feb9b864ef72c
SachinNagaraddi <sachinnagaraddi7781@gmail.com>
2026-07-26 21:48:24 +0530

    Update Register.v

--- src/Register.v
+++ src/Register.v
@@ -11,26 +11,24 @@ module Register (
     output [31:0] readData1,
     output [31:0] readData2
 );
-    reg [31:0] regs [0:31];
-
-// Do not modify this file!
-    assign readData1 = (readReg1 != 5'd0) ? regs[readReg1] : 32'd0;
-    assign readData2 = (readReg2!=0)?regs[readReg2]:0;
+    reg [31:0] regs [31:0];
+    integer i;
+    //Read
+    assign readData1 = regs[readReg1];
+    assign readData2 = regs[readReg2];
      
     always @(posedge clk) begin
-        if(~rst) begin
-            regs[0] <= 0; regs[1] <= 0; regs[2] <= 32'd128; regs[3] <= 0; 
-            regs[4] <= 0; regs[5] <= 0; regs[6] <= 0; regs[7] <= 0; 
-            regs[8] <= 0; regs[9] <= 0; regs[10] <= 0; regs[11] <= 0; 
-            regs[12] <= 0; regs[13] <= 0; regs[14] <= 0; regs[15] <= 0; 
-            regs[16] <= 0; regs[17] <= 0; regs[18] <= 0; regs[19] <= 0; 
-            regs[20] <= 0; regs[21] <= 0; regs[22] <= 0; regs[23] <= 0; 
-            regs[24] <= 0; regs[25] <= 0; regs[26] <= 0; regs[27] <= 0; 
-            regs[28] <= 0; regs[29] <= 0; regs[30] <= 0; regs[31] <= 0;        
+        if(rst) begin
+            for(i=0;i<32;i=i+1)begin
+                regs[i]<=0;
+            end
         end
-        else if(regWrite)
-            regs[writeReg] <= (writeReg == 0) ? 0 : writeData;
     end
-
+    //Write 
+    always@(negedge clk)begin
+        if(regWrite && writeReg!=0)begin
+            regs[writeReg]<=writeData;
+        end
+    end
 endmodule
 
