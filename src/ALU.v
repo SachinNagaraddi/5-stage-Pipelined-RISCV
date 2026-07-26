1a9f6071d3410826d091ec262b752f6a9899238d
SachinNagaraddi <sachinnagaraddi7781@gmail.com>
2026-07-26 22:29:41 +0530

    Update ALU.v

--- src/ALU.v
+++ src/ALU.v
@@ -4,24 +4,23 @@ module ALU (
     input [3:0] ALU_ctrl,
 
     output reg [31:0] result,
-    output zero
+    output zero,overflow,negative,
+    output reg carry
 );
-
-// ALU operations for RISC pipeline
-
 always @(*) begin
-    result = 32'b0; // default
-
+    carry=0;
     case(ALU_ctrl)
-        4'b0010: result = A + B;       // addition
-        4'b0110: result = A - B;       // subtraction
+        4'b0010: {carry,result} = A + B;       // addition
+        4'b0110: {carry,result} = A - B;       // subtraction
         4'b0000: result = A & B;       // and
         4'b0001: result = A | B;       // or
-        4'b0111: result = (A < B);     // set less than
+        4'b0111: result = ($signed(A) < $signed(B))? 32'd1 : 32'd0;     // set less than
         default: result = 32'b0;
     endcase
 end
 
-assign zero = (result == 0);
+assign zero=(result == 0);
+assign negative=result[31];
+assign overflow=(~ALU_ctrl[2])&(A[31]^result[31])&(~(ALU_ctrl[2]^A[31]^B[31]));
 
 endmodule
