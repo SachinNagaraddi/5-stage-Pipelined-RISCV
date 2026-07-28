bbd17631867b59a0d0747e79fae9561d6cf5b24f
SachinNagaraddi <sachinnagaraddi7781@gmail.com>
2026-07-28 23:25:27 +0530

    Update pc.v

--- src/pc.v
+++ src/pc.v
@@ -1,6 +1,7 @@
 module pc (
     input clk,
     input reset,
+    input stall,
     input [31:0] pc_next,
     output reg [31:0] pc
 );
@@ -8,7 +9,7 @@ module pc (
 always @(posedge clk or posedge reset) begin
     if (reset)
         pc <= 32'b0;       // start from address 0
-    else
+    else if(!stall)
         pc <= pc_next;
 end
 
