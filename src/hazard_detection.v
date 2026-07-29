88fd42189720a1407c5d8cdfdd25f76f313fa841
SachinNagaraddi <sachinnagaraddi7781@gmail.com>
2026-07-29 18:43:34 +0530

    Update hazard_detection.v

--- src/hazard_detection.v
+++ src/hazard_detection.v
@@ -1,23 +1,26 @@
-module hazard_detection(
-    input MemRead,
-    input [4:0] ID_EX_rd,
-    input [4:0] IF_ID_rs1,
-    input [4:0] IF_ID_rs2,
-    output reg PCWrite,
-    output reg IF_ID_Write,
-    output reg control_sel
-);
+module hazard_unit(
+  input [4:0] IF_ID_rs1,IF_ID_rs2,ID_EX_rd,
+  input ID_EX_MemRead,Branch,zero
+  output reg  stall,flush,flushE,PCSrc
+);  
+    always @(*) begin
+        if (ID_EX_MemRead && (ID_EX_rd!=5'd0) && ((ID_EX_rd==IF_ID_rs1)||(ID_EX_rd==IF_ID_rs2)))
+            stall = 1'b1;
+        else
+            stall = 1'b0;
 
-always @(*) begin
-    if (MemRead && ((ID_EX_rd == IF_ID_rs1) || (ID_EX_rd == IF_ID_rs2))) begin
-        PCWrite = 0;
-        IF_ID_Write = 0;
-        control_sel = 1;
-    end else begin
-        PCWrite = 1;
-        IF_ID_Write = 1;
-        control_sel = 0;
+        if(branch && zero)begin
+            flush=1'b1;
+            PCSrc=1'b1;
+        end
+        else begin
+            flush=0;
+            PCSrc=0;
+        end
+
+        if(PCSrc || stall)
+            flushE=1'b1;
+        else
+            flushE=0;
     end
-end
-
 endmodule
