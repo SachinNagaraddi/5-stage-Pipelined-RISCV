a5e8aca3f092a16a4287ddf12774551c3ec432bd
SachinNagaraddi <sachinnagaraddi7781@gmail.com>
2026-07-30 17:12:54 +0530

    Update InstructionMemory.v

--- src/InstructionMemory.v
+++ src/InstructionMemory.v
@@ -1,12 +1,13 @@
 module InstructionMemory (
     input [31:0] readAddr,
+    input reset,
     output [31:0] inst
 );
     
 
     reg [7:0] insts [127:0];
     
-    assign inst = (readAddr >= 125) ? 32'b0 : {insts[readAddr], insts[readAddr + 1], insts[readAddr + 2], insts[readAddr + 3]};
+    assign inst = (reset)? 32'h00000013:(readAddr >= 125)? 32'h00000013: {insts[readAddr], insts[readAddr + 1], insts[readAddr + 2], insts[readAddr + 3]};
 
     initial begin
         $readmemb("TEST_INSTRUCTIONS.dat", insts);
